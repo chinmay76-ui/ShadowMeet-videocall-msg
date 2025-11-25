d56d62d470db4dd10ec731699611733cf8a1c678
chinmay76-ui <daschinmaya610@gmail.com>
2025-11-25 10:59:19 +0530

    commit

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -1,9 +1,36 @@
-import nodemailer from "nodemailer";
+// utils/sendEmail.js
+import axios from "axios";
 
-export const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+export const transporter = {
+  sendMail: async ({ to, subject, html }) => {
+    try {
+      const response = await axios.post(
+        "https://api.brevo.com/v3/smtp/email",
+        {
+          sender: { 
+            email: "lmsbc56@gmail.com", 
+            name: "ShadowMeet" 
+          },
+          to: [{ email: to }],
+          subject,
+          htmlContent: html,
+        },
+        {
+          headers: {
+            "api-key": process.env.BREVO_API_KEY,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      console.log("Brevo email sent:", response.data);
+      return response.data;
+    } catch (error) {
+      console.error(
+        "Brevo sendMail error:",
+        error.response?.data || error
+      );
+      throw new Error("Email sending failed");
+    }
   },
-});
+};
